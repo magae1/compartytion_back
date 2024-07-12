d2a8172b8c6724938756cc88f66b06f5d7985e1a
magae1 <jjilil6665@gmail.com>
2024-07-13 01:53:18 +0900

    🎉 Begin django project

---
+++ src/compartytion/config/settings/local.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+
+MIDDLEWARE += [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_URLS_REGEX = r"^/api/.*$"
