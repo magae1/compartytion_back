fdd586a9a6d86be66bd16ee504180414e677c3af
magae1 <jjilil6665@gmail.com>
2024-07-26 01:06:11 +0900

    🔧 Fix media serve configuration

--- src/compartytion/config/urls.py
+++ src/compartytion/config/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.routers import SimpleRouter
 
 from rest_framework_simplejwt.views import (
@@ -36,7 +37,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("api/", include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
