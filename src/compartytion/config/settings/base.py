303a8f5fbb1f24149e67095880301a990cb1e382
magae1 <jjilil6665@gmail.com>
2024-08-01 01:20:46 +0900

    🚚 Repath static and configuration files

--- src/compartytion/config/settings/base.py
+++ src/compartytion/config/settings/base.py
@@ -15,10 +15,9 @@ from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
-load_dotenv(BASE_DIR.parent.parent.joinpath(".env"))
+load_dotenv(BASE_DIR.joinpath(".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
     "drf_spectacular",
     "corsheaders",
     "compartytion.users",
+    "compartytion.playground",
     "django_cleanup.apps.CleanupConfig",
 ]
 
--- src/compartytion/config/settings/local.py
+++ src/compartytion/config/settings/local.py
@@ -12,7 +12,7 @@ INTERNAL_IPS = ["127.0.0.1"]
 
 MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
 
-MEDIA_ROOT = os.path.join(BASE_DIR.parent.parent, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
